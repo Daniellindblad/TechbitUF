b9a8ac5be383b6dc4fde6bc528f7a498c275ca99
daniellindblad <daniel.lindblad99@hotmail.com>
2017-11-30 15:59:54 +0100

    gjort ett formulär

---
+++ form/index.php
@@ -0,0 +1,21 @@
+<form method="post" action="">
+
+Mottagare:<input type="text" name="mottagare"required></input><br>
+
+Produkt: <input type="text" name="produkt"required></input><br>
+
+Meddelande: <input type="text" name="meddelande"></input><br>
+
+Ansvarig: <input type="text" name="ansvarig"></input><br>
+
+Pris: <input type="text" name="pris"></input><br>
+
+Kund namn:<input type="text" name="kund_namn"required></input><br>
+
+Kontaktuppgifter:<input type="text" name="kontaktuppgifter"required></input><br>
+
+Extra information: <input type="text" name="extra"></input><br>
+
+<input type="submit">
+
+</form>
